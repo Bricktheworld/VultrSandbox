6eaa149b1fdcac582ea6504ffc24f552513694e9
Bricktheworld <brandon.shihabi@gmail.com>
2022-01-10 00:10:55 -0800

    Removed verbose output

--- src/sources.cpp
+++ src/sources.cpp
@@ -1,5 +1,5 @@
 #include <vultr.h>
 
 void use_game_memory(void *m) { Vultr::g_game_memory = static_cast<Vultr::GameMemory *>(m); }
-void vultr_init(void) { printf("Some init thing.\n"); }
-void vultr_update(void) { printf("Some update thing.\n"); }
+void vultr_init(void) {}
+void vultr_update(void) {}
