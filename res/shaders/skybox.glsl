50396c396b0bb8cc3d4659a7a75fbd885287ef29
Brandon Shihabi <brandon.shihabi@gmail.com>
2022-09-03 11:42:54 -0700

    Latest changes

--- res/shaders/skybox.glsl
+++ res/shaders/skybox.glsl
@@ -3,24 +3,25 @@
 layout (set = 1, binding = 0) uniform Skybox {
     vec4 unused;
 } u_Skybox;
-layout (set = 1, binding = 1) uniform sampler2D u_Albedo;
+layout (set = 1, binding = 1) uniform samplerCube u_Skybox_map;
 
 #[VERTEX]
-layout (location = 0) out vec2 f_UV;
+layout (location = 0) out vec3 f_UV;
 
 void main()
 {
-    f_UV = vec2(v_Position);
-    gl_Position = u_Camera.view_proj * vec4(v_Position, 1.0f);
+    f_UV = v_Position;
+		vec4 pos = u_Camera.proj * mat4(mat3(u_Camera.view)) * vec4(v_Position, 1.0f);
+    gl_Position = pos.xyww;
 }
 
 #[FRAGMENT]
 
 layout (location = 0) out vec4 color;
 
-layout (location = 0) in vec2 f_UV;
+layout (location = 0) in vec3 f_UV;
 
 void main()
 {
-    color = texture(u_Albedo, f_UV);
+    color = texture(u_Skybox_map, f_UV);
 }
